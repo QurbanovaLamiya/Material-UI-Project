a825e0f8cac6be654715704f639558f5d7fde506
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2023-02-24 15:45:21 +0400

    Update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,50 @@
-import { AppBar, styled, Toolbar } from "@mui/material";
+import { AppBar, styled, Toolbar, Box, InputBase, Badge } from "@mui/material";
 import AnimationIcon from "@mui/icons-material/Animation";
+import { Mail, Notifications } from "@mui/icons-material";
+import AccountMenu from "./Menu";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
 });
 
+const Search = styled("div")(({ theme }) => ({
+  background: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "20px",
+}));
+
 const Navbar = () => {
   return (
-    <AppBar position="sticky">
+    <AppBar position="sticky" sx={{ background: "#4db6ac" }}>
       <StyledToolbar>
-        <Toolbar variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+        <Toolbar
+          variant="h6"
+          component="h2"
+          sx={{ display: { xs: "none", sm: "block" } }}
+        >
           Navbar
         </Toolbar>
         <AnimationIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <AccountMenu />
+        </Icons>
       </StyledToolbar>
     </AppBar>
   );
